4b23239bb08b0fa709f5e95c2f53619751b1adad
unknown <armandev396@gmail.com>
2021-10-01 19:16:54 +0600

    last-change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Header from './components/Header/Header';
+import MyCode from './components/MyCode/MyCode';
 import Shop from './components/Shop/Shop';
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <MyCode></MyCode>
       <Shop></Shop>
+
     </div>
   );
 }
---
+++ src/components/MyCode/MyCode.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyCode = () => {
+    return (
+        <div>
+                <h1>hello bangladesh last</h1>
+        </div>
+    );
+};
+
+export default MyCode;
